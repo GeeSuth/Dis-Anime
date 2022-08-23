22f163a448e08f6475ed77c3b6d12c1528dbc405
GeeSuth <servecemodea@gmail.com>
2022-08-23 19:13:37 +0300

    change some ui

--- static/main.js
+++ static/main.js
@@ -97,10 +97,6 @@ async function DrawCard(data) {
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${row["title"]}</h5>
                 </a>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${row["filename"]}</p>
-                <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Read more
-                    <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </a>
             </div>
         </div>`);
             //console.log(card);
@@ -137,7 +133,7 @@ async function uploadImage() {
 
 
 
-/* Common  */
+/* ******************************************Common *********************************************************** */
 function pasteUrl() {
     input = document.getElementById('urlImg');
     navigator.clipboard
